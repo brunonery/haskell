0c0b98ac05d2cdd7d822e75dbb8466340e517180
Bruno Nery <bruno@thousandeyes.com>
2012-10-22 18:23:53 -0700

    Changed twitter.hs to use conduit explicitly.

--- twitter.hs
+++ twitter.hs
@@ -5,12 +5,20 @@
 -- And maybe about parser combinators.
 -- The next step is to try and make this conduit stream. A good test is to pipe
 -- the output to grep and see if it rolls the output or spits everything at once.
-import Char
+import Data.Conduit.Binary (sinkHandle)
+import IO (stdout)
 import Network.HTTP.Conduit
-import qualified Data.ByteString.Lazy as L
+import qualified Data.Conduit as C
+import qualified Data.Conduit.List as CL
+import qualified Data.ByteString as B
 import qualified Data.Word8 as W
 
-main =
-  simpleHttp "http://www.twitter.com" >>=
-  (return . L.pack . map W.toUpper . L.unpack) >>=
-  L.putStr
+main :: IO()
+main = do
+  request <- parseUrl "http://www.twitter.com/"
+  withManager $ \manager -> do
+    Response _ _ _ src <- http request manager
+    -- $$+- instead of $$ because 'src' is a ResumableSource (as per Conduit.hs)
+    src C.$$+- conduit C.=$ sinkHandle stdout
+      where
+        conduit = CL.map $ B.map W.toUpper
